d7d8c127a3bbe64074dc7f5f05aa56a8bee17988
Valdir Santos <valdirsantost40@gmail.com>
2023-12-23 22:27:33 -0300

    Adicionado função de porcentagem e raiz quadrada

--- src/aplicacao/App.java
+++ src/aplicacao/App.java
@@ -16,7 +16,7 @@ public class App {
             Calculadora calculadora = new Calculadora();
 
             System.out.println("Programa Calculadora");
-            System.out.print("Digite o primeiro número: ");
+            System.out.print("Digite o número: ");
             Double num1 = ler.nextDouble();
 
             ler.nextLine();
@@ -24,10 +24,28 @@ public class App {
             System.out.print("Digite a operação a ser realizada: ");
             String operacao = ler.nextLine();
 
-            System.out.print("Digite o segundo número: ");
-            Double num2 = ler.nextDouble();
+            if (operacao.equals("+") || operacao.equals("-") || operacao.equals("*") || operacao.equals("/")) {
 
-            calculadora.calcular(num1, operacao, num2);
+                System.out.print("Digite o segundo número: ");
+                Double num2 = ler.nextDouble();
+
+                calculadora.calcularFuncoesBasicas(num1, operacao, num2);
+
+            } else if (operacao.equals("v")) {
+                calculadora.raizQuadrada(num1);
+
+            } else if (operacao.equals("%")) {
+                //ler.nextLine();
+
+                System.out.println("Digite se vc quer aumentar ou diminuir esse valor (+ ou -)");
+                String operador = ler.nextLine();
+
+                System.out.println("Digite quantos porcento você quer que aumente: ");
+                int porcentagem = ler.nextInt();
+
+                calculadora.porcentagem(num1, operador, porcentagem);
+            }
+
 
         } catch (CalculadoraExcessoes e) {
             System.out.println(e.getMessage());
--- src/controle/Calculadora.java
+++ src/controle/Calculadora.java
@@ -9,26 +9,21 @@ public class Calculadora {
     public Calculadora() {
     }
 
-    public void calcular(Double n1, String operacao ,Double n2) {
-        double resultado = 0.0;
+    public void calcularFuncoesBasicas(Double n1, String operacao ,Double n2) {
+
         try {
 
             if (operacao.equals("+")) {
-                resultado = n1 + n2;
+                somar(n1,n2);
 
             } else if (operacao.equals("-")) {
-                if (n1 >= n2) {
-                    resultado = n1 - n2;
-
-                } else {
-                    resultado = n2 - n1;
-                }
+               subtrair(n1, n2);
 
             } else if (operacao.equals("*")) {
-                resultado = n1 * n2;
+                multiplicar(n1, n2);
 
             } else if (operacao.equals("/")) {
-                resultado = n1 / n2;
+                dividir(n1,n2);
 
             } else {
                 throw new CalculadoraExcessoes(" Erro! Digite um sinal de operação matemática válido ( +, -, *, / )");
@@ -38,8 +33,44 @@ public class Calculadora {
         } catch (NumberFormatException | InputMismatchException e) {
             throw new CalculadoraExcessoes(e.getMessage());
 
-        } finally {
-            System.out.format("%.2f", resultado);
         }
+    }
+
+    public void somar(Double n1, Double n2) {
+
+        System.out.println(String.format(n1 + " + " + n2 + " = %.2f", n1 + n2));
+    }
+
+    public void subtrair(Double n1, Double n2) {
+        System.out.println(String.format(n1 + " - " + n2 + " = %.2f", n1 - n2));
+
+    }
+
+    public void multiplicar(Double n1, Double n2) {
+        System.out.println(String.format(n1 + " * " + n2 + " = %.2f", n1 * n2));
+    }
+
+    public void dividir(Double n1, Double n2) {
+        System.out.println(String.format(n1 + " / " + n2 + " = %.2f", n1 / n2));
+    }
+
+    public void raizQuadrada(Double n) {
+        System.out.println(String.format("A raiz quadrada de " + n + " é %.2f", Math.sqrt(n)));
+    }
+
+    public void porcentagem(Double n, String operacao, int porcentagem) {
+        //ajeitar o menos
+        double resultado = 0.0;
+        if (operacao.equals("+")) {
+            resultado = (n / 100) * porcentagem + n;
+            System.out.println(String.format("%.2f", n) + " + " + porcentagem + "% " + "= " + String.format("%.2f", resultado));
+
+        } else if (operacao.equals("-")) {
+            resultado = (n / 100) * porcentagem - n;
+            System.out.println(String.format("%.2f", n) + " - " + porcentagem + "% " + "= " + String.format("%.2f", resultado));
+
+        }
+
+
     }
 }
